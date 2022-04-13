cb63527cb96784c49c395d0b6acb591f11cd5100
rrich-kray <rrich.kray@gmail.com>
2022-04-13 19:38:44 -0400

    revamped README file and added LICENSE file

--- README.md
+++ README.md
@@ -1,39 +1,79 @@
 # Weather Dashboard
 
-## The Repository
-
 This repository contains the HTML, CSS and Javascript code for the Weather Dashboard Project.
 
-## Weather Dashboard
+## Table of Contents
+
+[description](#description)  
+[features](#features)  
+[how it works](#how-it-works)
+[installation](#installation)  
+[contact](#contact)
+[license](#license)
+
+## Description
 
 ![Weather Dashboard Screenshot](./assets/images/screen1.png)
 
-Weather dashboard makes it easy to obtain current and forecasted weather conditions for a desired city.  
+Weather dashboard was created so that users can obtain weather for any city in the United States. While this served as a great introductory experience to third-party APIs, I believe there exists significant room for improvement in this application, including:
 
-## Usage
+- Refactoring the codebase to adhere more closely to the SOLID framework for better readability and maintainability.
+- Fixing formatting in some areas to prevent element overlap and to make the application more mobile-friendly. Also, the format in which dates are displayed could be more user-friendly.
+- Expanded customization for users. For example, in its current build, the application only allows users to view a seven day forecast.
+- More robust user input validation.
 
-Users can pull weather data by using the search box shown below. Please note that input therein must in [city], [state] format.
+## Features
 
-User searches are saved in the browser's local storage and displayed directly below the search box and button. Clicking on any of these past searches will pull the current and forecasted weather data for the past search.
+In its current build, Weather Dashboard allows users to:
 
-The "Clear Searches" button located directly below the search button clears local storage of any past user searches. 
+- Fetch data from the Open Weather Map API
+- View a seven-day weather forecast for a city of their choosing
+- View temperature, wind, UVI and humidity data for each day in the forecast
+
+## How it Works
+
+When a user submits a search, an event listener fires that calls a series of functions that perform the following actions:
+
+- User input is taken from the search box element and separated into city and state components. These are then formatted with string methods.
+- User input is then saved into local storage. A separate function reads local storage and displays past search history below the search bar.
+- User input is then integrated into an Open Weather Map url that is used to fetch data from the API.
+- This data is then displayed in tile format.
+
+The application's search box and search results resemble the following, respectively:
 
 ![Search Box Screenshot](./assets/images/screen2.png)
-
-Clicking the search button will populate the current conditions section with weather data pertaining to the user's search, as well as span a weather tile for each of the next seven days.
-
 ![Search Results Screenshot](./assets/images/screen3.png)
 
-## Deployed Application
+In addition, UV index data is conditionally formatted based on its severity. Users also have the option to clear search history by selecting the "clear" button adjacent to the "search" button.
 
-the deployed application can be found by following the link below.
+## Installation
+
+No installation required. Simply follow the link below:
 
 https://rrich-kray.github.io/weather-dashboard/
 
-## Feedback
+## Contact
 
-Leave comments/suggestions in the Slack channel!
+GitHub profile:
+
+https://github.com/rrich-kray/
+
+Email inquiries:
+
+rrich.kray.93@gmail.com
 
 ## License
 
-Copyright &copy; Weather Dashboard, Inc. All rights reserved.
+Copyright (c) 2022, Ryan Kray
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree and reproduced below.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+3. All advertising materials mentioning features or use of this software must display the following acknowledgement: This product includes software developed by Ryan Kray.
+4. Neither the name of this application's developer nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,102 +1,130 @@
 var searchedTerms = [];
 var searchBtn = document.querySelector(".search-btn");
-var clearResultsBtn = document.querySelector('.clear-results-btn')
-var key = 'ce8ded9363f8838690bb46ae0314a5c5'
+var clearResultsBtn = document.querySelector(".clear-results-btn");
+var key = "ce8ded9363f8838690bb46ae0314a5c5";
 
-var checkStorage = function(){
-    if (!window.localStorage.getItem('searches')) {
-        window.localStorage.setItem("searches", JSON.stringify(searchedTerms))
-    }
-}
+var checkStorage = function () {
+  if (!window.localStorage.getItem("searches")) {
+    window.localStorage.setItem("searches", JSON.stringify(searchedTerms));
+  }
+};
 
 // This function is called when the user clicks a past search. Takes the innerHTML of the button, splits it using a comma delimiter, trims the output, and passes the output as arguments to the pullCityData function, along with the api key
-var getBtnText = function(element) {
-    var btnTerms = element.innerHTML.split(',');
-    const [ city, state ] = btnTerms;
-    pullCityData(city, state, key);
-}
+var getBtnText = function (element) {
+  var btnTerms = element.innerHTML.split(",");
+  const [city, state] = btnTerms;
+  pullCityData(city, state, key);
+};
 
-var getUserInput = function(){
-    // Capture user input and split it by city name and state name; trim spaces and convert stateCode to uppercase only, and add "US-" so that it adheres to ISO-3166 standards
-    var userInput = document.querySelector('.search').value.split(',');
-    if ((userInput.length < 2 || userInput[1].trim().length > 2)) {
-        alert("Please enter the city name and two-letter state code");
-        return;
-    }
-    var [ city, state ] = userInput;
-    city = city.trim()
-    state = `US-${state.trim().toUpperCase()}`
-    saveUserInput(city, state);
-    pullCityData(city, state, key);
-}
+var getUserInput = function () {
+  // Capture user input and split it by city name and state name; trim spaces and convert stateCode to uppercase only, and add "US-" so that it adheres to ISO-3166 standards
+  var userInput = document.querySelector(".search").value.split(",");
+  if (userInput.length < 2 || userInput[1].trim().length > 2) {
+    alert("Please enter the city name and two-letter state code");
+    return;
+  }
+  var [city, state] = userInput;
+  city = city.trim();
+  state = `US-${state.trim().toUpperCase()}`;
+  saveUserInput(city, state);
+  pullCityData(city, state, key);
+};
 
-var saveUserInput = function(cityName, stateCode) {
-    // Include search in local storage only if search does not already exist
-    var searchedTerms = JSON.parse(window.localStorage.getItem('searches'))
-    console.log(searchedTerms);
-    if (!searchedTerms.includes([cityName, stateCode])) {
-        searchedTerms.push([cityName, stateCode])
-    }
-    window.localStorage.setItem('searches', JSON.stringify(searchedTerms))
-}
+var saveUserInput = function (cityName, stateCode) {
+  // Include search in local storage only if search does not already exist
+  var searchedTerms = JSON.parse(window.localStorage.getItem("searches"));
+  console.log(searchedTerms);
+  if (!searchedTerms.includes([cityName, stateCode])) {
+    searchedTerms.push([cityName, stateCode]);
+  }
+  window.localStorage.setItem("searches", JSON.stringify(searchedTerms));
+};
 
-var pullCityData = function(cityName, stateCode, apiKey) {
+var pullCityData = function (cityName, stateCode, apiKey) {
+  // Display searches on page
+  displaySearches();
 
-    // Display searches on page
-    displaySearches();
-
-    // Insert user input into weather api. Open Weather Api has convenient "Onecall" api with current and forecast data but, oddly, only accepts lat and lon values.
-    // So, this takes the user's search and uses it to extract lat and lon values from separate api, then inserts into the onecall api url
-    var api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}&appid=${apiKey}`
-    fetch(api).then(response => {if (response.ok) {response.json().then(data => {
-        var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude={part}&units=imperial&appid=${apiKey}`
-        fetch(oneCallApi).then(response => {if (response.ok) {response.json().then(data => {
-            console.log(data)
-            displayCurrentWeatherData(data);
-            displayWeatherForecast(data)})}})
-            })
-        } else {
-            alert(`Error ${response.statusText}`)
-        }
+  // Insert user input into weather api. Open Weather Api has convenient "Onecall" api with current and forecast data but, oddly, only accepts lat and lon values.
+  // So, this takes the user's search and uses it to extract lat and lon values from separate api, then inserts into the onecall api url
+  var api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}&appid=${apiKey}`;
+  fetch(api)
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude={part}&units=imperial&appid=${apiKey}`;
+          fetch(oneCallApi).then((response) => {
+            if (response.ok) {
+              response.json().then((data) => {
+                console.log(data);
+                displayCurrentWeatherData(data);
+                displayWeatherForecast(data);
+              });
+            }
+          });
+        });
+      } else {
+        alert(`Error ${response.statusText}`);
+      }
     })
-    .catch(function(){
-        alert("No data found!");
-    })
-}
+    .catch(function () {
+      alert("No data found!");
+    });
+};
 
-// Displays the current conditions in the .weather-data-container div; 
-var displayCurrentWeatherData = function(weatherData){
-    document.querySelector('#temp').innerHTML = `${weatherData.current.temp} ℉`
-    document.querySelector('#wind').innerHTML = `${weatherData.current.wind_speed} mph`;
-    document.querySelector('#humidity').innerHTML = `${weatherData.current.humidity} g.kg^-1`;
-    document.querySelector('#uv-index').innerHTML = `${weatherData.current.uvi} %rh`;
-}
+// Displays the current conditions in the .weather-data-container div;
+var displayCurrentWeatherData = function (weatherData) {
+  document.querySelector("#temp").innerHTML = `${weatherData.current.temp} ℉`;
+  document.querySelector(
+    "#wind"
+  ).innerHTML = `${weatherData.current.wind_speed} mph`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `${weatherData.current.humidity} g.kg^-1`;
+  document.querySelector(
+    "#uv-index"
+  ).innerHTML = `${weatherData.current.uvi} %rh`;
+};
 
-var displayWeatherForecast = function(weatherData){
-    // check if data was pulled 
-    if (weatherData.length === 0) {document.querySelector('.tile-container').innerHTML = "No data found"}
-    var tileContainer = document.querySelector('.tile-container');
-    //clear tileContainer innerHTML so that weather tiles do not pile up
-    tileContainer.innerHTML = '';
-    var counter1 = 1;
-    // var counter2 = 1;
-    weatherData.daily.forEach(function(day) {
-        var thumbnail;
-        if (day.weather[0]['main'].toLowerCase() === 'rain') {thumbnail = './assets/images/storm-day.svg'};
-        if (day.weather[0]['main'].toLowerCase() === 'clouds') {thumbnail = './assets/images/cloudy-day.svg'};
-        if (day.weather[0]['main'].toLowerCase() === 'snow') {thumbnail = './assets/images/snow.svg'};
-        if (day.weather[0]['main'].toLowerCase() === 'clear') {thumbnail = './assets/images/sunny-day.svg'};
-        var html = `
+var displayWeatherForecast = function (weatherData) {
+  // check if data was pulled
+  if (weatherData.length === 0) {
+    document.querySelector(".tile-container").innerHTML = "No data found";
+  }
+  var tileContainer = document.querySelector(".tile-container");
+  //clear tileContainer innerHTML so that weather tiles do not pile up
+  tileContainer.innerHTML = "";
+  var counter1 = 1;
+  // var counter2 = 1;
+  weatherData.daily.forEach(function (day) {
+    var thumbnail;
+    if (day.weather[0]["main"].toLowerCase() === "rain") {
+      thumbnail = "./assets/images/storm-day.svg";
+    }
+    if (day.weather[0]["main"].toLowerCase() === "clouds") {
+      thumbnail = "./assets/images/cloudy-day.svg";
+    }
+    if (day.weather[0]["main"].toLowerCase() === "snow") {
+      thumbnail = "./assets/images/snow.svg";
+    }
+    if (day.weather[0]["main"].toLowerCase() === "clear") {
+      thumbnail = "./assets/images/sunny-day.svg";
+    }
+    var html = `
         
         <div class="tile">
         
             <div class="front">
                 <img class="thumbnail" src=${thumbnail} alt="">
-                <h5 class="date">${moment(moment(), "MM-DD-YYYY").add(counter1, 'days')}</h5> <!--date can go here-->
+                <h5 class="date">${moment(moment(), "MM-DD-YYYY").add(
+                  counter1,
+                  "days"
+                )}</h5> <!--date can go here-->
                 <div class="changing-container">
                     <p>Temp: <span class="temp">${day.temp.day} ℉</span></p>
                     <p>Wind: <span class="wind">${day.wind_speed} mph</span></p>
-                    <p>Humidity: <span class="humidity">${day.humidity} %rh</span></p>
+                    <p>Humidity: <span class="humidity">${
+                      day.humidity
+                    } %rh</span></p>
                     <p>UVI: <span class="UVI">${day.uvi} g.kg^-1</span></p>
                 </div>
             </div>
@@ -105,58 +133,53 @@ var displayWeatherForecast = function(weatherData){
 
         </div>
             
-            `
+            `;
 
-        // `
-        //     <div class="weather-tile tile${counter2} flex-column a-left j-center j-space-between">
-        //         <img class="weather-icon" />
-        //         <p>Date: <span id="date">${date.addDays(counter1)}</span></p>
-        //         <p>Temperature: <span id="temp">${day.temp.day} ℉</span></p>
-        //         <p>Wind: <span id="wind">${day.wind_speed} mph</span></p>
-        //         <p>Humidity: <span id="humidity">${day.humidity} %rh</span></p>
-        //     </div>`
-        //     ;
-            
-        tileContainer.innerHTML += html;
-        counter1++;
-        // counter2++;
-    })
-}
+    tileContainer.innerHTML += html;
+    counter1++;
+    // counter2++;
+  });
+};
 
-// Clears past searches by pulling search history from local storage and setting array value to empty. 
-//displaySearches() function is then evoked, which also clears the innerHTML of the search area to align it with what's in local storage 
-var clearResults = function() {
-    var searchedTerms = JSON.parse(window.localStorage.getItem('searches'));
-    searchedTerms = [];
-    window.localStorage.setItem('searches', JSON.stringify(searchedTerms));
-    displaySearches();
-}
+// Clears past searches by pulling search history from local storage and setting array value to empty.
+//displaySearches() function is then evoked, which also clears the innerHTML of the search area to align it with what's in local storage
+var clearResults = function () {
+  var searchedTerms = JSON.parse(window.localStorage.getItem("searches"));
+  searchedTerms = [];
+  window.localStorage.setItem("searches", JSON.stringify(searchedTerms));
+  displaySearches();
+};
 
 // pulls search history from local storage and creates button for each term pulled
-var displaySearches = function(){
-    document.querySelector('.search-results').innerHTML = '';
-    var searchedTerms = JSON.parse(window.localStorage.getItem('searches'));
-    searchedTerms.forEach(function(term){
-        var city = term[0];
-        var state = term[1];
-        var html = `<button class="result-btn flex-row j-center a-center">${city}, ${state}</button>`
-        document.querySelector('.search-results').innerHTML += html
-    })
-}
+var displaySearches = function () {
+  document.querySelector(".search-results").innerHTML = "";
+  var searchedTerms = JSON.parse(window.localStorage.getItem("searches"));
+  searchedTerms.forEach(function (term) {
+    var city = term[0];
+    var state = term[1];
+    var html = `<button class="result-btn flex-row j-center a-center">${city}, ${state}</button>`;
+    document.querySelector(".search-results").innerHTML += html;
+  });
+};
 
-var colorUvIndex = function(element){
-    if (element.innerHTML <= 2) {element.style.backgroundColor = "green"};
-    if (element.innerHTML > 2 && element.innerHTML <= 5) {element.style.backgroundColor = "yellow"};
-    if (element.innerHTML > 5) {element.style.backgroundColor = "red"};
-}
+var colorUvIndex = function (element) {
+  if (element.innerHTML <= 2) {
+    element.style.backgroundColor = "green";
+  }
+  if (element.innerHTML > 2 && element.innerHTML <= 5) {
+    element.style.backgroundColor = "yellow";
+  }
+  if (element.innerHTML > 5) {
+    element.style.backgroundColor = "red";
+  }
+};
 
 checkStorage();
 window.onload = displaySearches();
-clearResultsBtn.addEventListener('click', clearResults);
-searchBtn.addEventListener('click', getUserInput);
-document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('result-btn')) {getBtnText(event.target)}
+clearResultsBtn.addEventListener("click", clearResults);
+searchBtn.addEventListener("click", getUserInput);
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("result-btn")) {
+    getBtnText(event.target);
+  }
 });
-
-
-
